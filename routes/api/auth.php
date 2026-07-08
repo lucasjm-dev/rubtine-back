1b452e942f02fdf8910ca4826ba2cd1a01750a46
lucasjm-dev <lucasmaldonado100@gmail.com>
2026-07-08 19:38:53 -0300

    Fix dont register at the moment

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
 
-    Route::post('register', [AuthController::class, 'register']);
+    // Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 
     Route::middleware('auth:api')->group(function () {
